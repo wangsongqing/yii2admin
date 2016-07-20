d473f59e93ca6994560b3e4e77d9b9be77e71f84
Your Name <you@example.com>
2016-07-20 11:23:21 +0800

    add

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'y4V0qknBcQeLBMbcz0j8bw6QxlcNX0wE',
+        ],
+    ],
+];
