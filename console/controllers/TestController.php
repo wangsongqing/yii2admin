f408436fe66c50a7c1f0ab86795e1c43debc88e0
Your Name <you@example.com>
2016-08-24 11:38:17 +0800

    yii2 控制台的使用示例

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -1,11 +1,19 @@
 <?php 
 namespace console\controllers;
+use Yii;
 use yii\console\Controller;
+use app\models\Lender;
  
 class TestController extends Controller
 {
     public function actionTest()
     {
-        echo "test ok\n";
+//        $connection = Yii::$app->db;
+//        $sql = "select * from admin";
+//        $command = $connection->createCommand($sql);
+//        $res = $command->queryOne();
+//        echo $res['username'];
+        $model = Lender::findOne('1');
+        echo $model->userName;
     }
 }
