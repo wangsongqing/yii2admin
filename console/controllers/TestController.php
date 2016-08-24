467a875d54b18b2aae25552f1de4237a47958741
Your Name <you@example.com>
2016-08-24 11:40:57 +0800

    yii2 控制台的使用示例

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -5,7 +5,10 @@ use yii\console\Controller;
 use app\models\Lender;
  
 class TestController extends Controller
-{
+{   
+    /**
+     * 调用方式  在项目目录下面找见文件名为yii的文件   F:\projects\yii2admin>yii test/test  
+     */
     public function actionTest()
     {
 //        $connection = Yii::$app->db;
