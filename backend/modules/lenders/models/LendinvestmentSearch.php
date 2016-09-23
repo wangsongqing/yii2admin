c1cf7bad05526fe67a842f8adf945f4de504aea0
Your Name <you@example.com>
2016-09-23 14:38:56 +0800

    page 10 of the data

--- backend/modules/lenders/models/LendinvestmentSearch.php
+++ backend/modules/lenders/models/LendinvestmentSearch.php
@@ -48,6 +48,7 @@ class LendinvestmentSearch extends Lendinvestment
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => ['pageSize' => 10], //设置每个页面显示几条数据
         ]);
 
         $this->load($params);
